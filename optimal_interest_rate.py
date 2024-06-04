32228f5838e24ad8afd9a53ab8512d38fc1d814e
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2024-06-03 23:01:15 -0500

    Finds optimal interest using bisection search

--- optimal_interest_rate.py
+++ optimal_interest_rate.py
@@ -1,26 +1,10 @@
-## 6.100A PSet 1: Part C
-## Name:
-## Time Spent:
-## Collaborators:
-
-##############################################
-## Get user input for initial_deposit below ##
-##############################################
 initial_deposit = float(input("Enter the initial deposit: "))
-
-#########################################################################
-## Initialize other variables you need (if any) for your program below ##
-#########################################################################
 cost = 800000
 down_payment = cost * 0.25
 low = 0.0
 high = 1.0
 tolerance = 100  # Within $100 of the down payment
 steps = 0
-
-##################################################################################################
-## Determine the lowest rate of return needed to get the down payment for your dream home below ##
-##################################################################################################
 if initial_deposit >= down_payment:
     r = 0.0
 else:
